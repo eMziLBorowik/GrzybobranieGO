01435e72c950c6c38a8fd5126103c1679d306d1b
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-19 00:46:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,38 +1,18 @@
 window.onload=function(){
 
 
-document.getElementById("tabDex").onclick=function(){
 
-document.getElementById("map").style.display="none";
-
-document.getElementById("grzybdex").style.display="block";
-
-updateStats();
-
-};
-
-
-
-document.getElementById("tabMap").onclick=function(){
-
-document.getElementById("map").style.display="block";
-
-document.getElementById("grzybdex").style.display="none";
-
-setTimeout(()=>{
-
-map.invalidateSize();
-
-},200);
-
-};
-
-
+// GPS
 
 
 if(!navigator.geolocation){
 
-document.getElementById("forestStatus").innerHTML="❌ Brak GPS";
+
+document.getElementById(
+"forestStatus"
+).innerText=
+"❌ Brak GPS";
+
 
 return;
 
@@ -42,7 +22,10 @@ return;
 
 navigator.geolocation.watchPosition(
 
-function(pos){
+
+
+(pos)=>{
+
 
 
 userLat=pos.coords.latitude;
@@ -50,8 +33,12 @@ userLat=pos.coords.latitude;
 userLng=pos.coords.longitude;
 
 
-document.getElementById("forestStatus").innerHTML=
+
+document.getElementById(
+"forestStatus"
+).innerText=
 "📍 GPS OK";
+
 
 
 
@@ -80,20 +67,20 @@ userMarker.setLatLng(
 );
 
 
+
 }
 
 
 
-if(typeof loadForests==="function"){
-
 if(forests.length===0){
+
 
 loadForests(
 userLat,
 userLng
 );
 
-}
+
 
 }
 
@@ -102,17 +89,18 @@ userLng
 },
 
 
-function(error){
+
+(err)=>{
 
 
-document.getElementById("forestStatus").innerHTML=
-"❌ GPS zablokowany";
-
-
-console.log(error);
+document.getElementById(
+"forestStatus"
+).innerText=
+"❌ GPS błąd";
 
 
 },
+
 
 
 {
@@ -122,7 +110,75 @@ maximumAge:0
 }
 
 
+
 );
+
+
+
+
+
+// zakładka mapa
+
+
+document.getElementById(
+"tabMap"
+).onclick=function(){
+
+
+
+document.getElementById(
+"map"
+).style.display="block";
+
+
+
+document.getElementById(
+"grzybdex"
+).style.display="none";
+
+
+
+setTimeout(()=>{
+
+map.invalidateSize();
+
+},300);
+
+
+
+};
+
+
+
+
+
+// zakładka grzybdex
+
+
+document.getElementById(
+"tabDex"
+).onclick=function(){
+
+
+
+document.getElementById(
+"map"
+).style.display="none";
+
+
+
+document.getElementById(
+"grzybdex"
+).style.display="block";
+
+
+
+updateStats();
+
+
+
+};
+
 
 
 
