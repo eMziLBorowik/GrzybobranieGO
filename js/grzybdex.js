e8dec2c24dc2cd1f6a7c339147fee98a8ec2fdd7
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-19 00:12:53 +0200

    Update grzybdex.js

--- js/grzybdex.js
+++ js/grzybdex.js
@@ -1,1 +1,9 @@
-document.getElementById('aiBtn').onclick=()=>document.getElementById('aiResult').innerText='🍄 Wynik AI';
+document.getElementById('aiBtn').onclick=()=>{
+const t=[
+'🍄 Borowik szlachetny (JADALNY)',
+'🍄 Podgrzybek (JADALNY)',
+'⚠️ Muchomor czerwony (TRUJĄCY)',
+'☠️ Muchomor sromotnikowy'
+];
+document.getElementById('aiResult').innerText=t[Math.floor(Math.random()*t.length)];
+};
