b2821cbbff56c9f5a84024aa0b41088c5237a628
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-19 21:42:02 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,4 +2,4 @@ const supabaseUrl = "https://kdcikplyzqyckvjkhusw.supabase.co";
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkY2lrcGx5enF5Y2t2amtodXN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4MTMwMDQsImV4cCI6MjA5NzM4OTAwNH0.u0mP8hdJ19erDGpplahdzaswSTsJrDLzG7c4ompPbzU";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
