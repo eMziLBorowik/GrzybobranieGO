c1a5f9c2f3d3a0fe94c76df43b03097e4e14c7b6
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-19 22:47:52 +0200

    Update mushrooms.js

--- js/mushrooms.js
+++ js/mushrooms.js
@@ -1,1 +1,21 @@
-window.mushroomsAtlas = window.mushroomsAtlas || [];
+window.mushroomsAtlas = [
+{ name:"Borowik szlachetny", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Podgrzybek brunatny", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Koźlarz czerwony", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Koźlarz babka", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Maślak zwyczajny", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Pieprznik jadalny (kurka)", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Czubajka kania", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Opieńka miodowa", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Mleczaj rydz (rydz)", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Gąska zielonka", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Gąska siwa", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Mleczaj świerkowy", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Pieczarka polna", type:"Jadalny", icon:"🍄", found:false },
+{ name:"Pieczarka leśna", type:"Jadalny", icon:"🍄", found:false },
+
+{ name:"Muchomor czerwony", type:"Trujący", icon:"⚠️", found:false },
+{ name:"Muchomor plamisty", type:"Trujący", icon:"⚠️", found:false },
+{ name:"Borowik szatański", type:"Trujący", icon:"⚠️", found:false },
+{ name:"Muchomor sromotnikowy", type:"Śmiertelnie trujący", icon:"☠️", found:false }
+];
