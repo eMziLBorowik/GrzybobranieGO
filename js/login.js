8d55e4b86cc3f06c136d671cdaf0f7dc904f3dfc
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-19 21:38:55 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,49 +1,49 @@
-const authPanel = document.getElementById("authPanel");
+const client = window.supabaseClient;
 
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
 const msg = document.getElementById("authMsg");
 
-document.getElementById("loginBtn").addEventListener("click", async ()=>{
+const panel = document.getElementById("authPanel");
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
 
 const email = emailInput.value;
 const password = passwordInput.value;
 
-const { data, error } = await supabase.auth.signInWithPassword({
+const { data, error } = await client.auth.signInWithPassword({
 email,
 password
 });
 
 if(error){
-msg.innerText = "❌ Błąd logowania";
+console.log(error);
+msg.innerText = "❌ Błąd logowania: " + error.message;
 return;
 }
 
 msg.innerText = "✅ Zalogowano";
 
-authPanel.style.display = "none";
-
-renderAtlas();
+panel.style.display = "none";
 
 });
 
-
-document.getElementById("registerBtn").addEventListener("click", async ()=>{
+document.getElementById("registerBtn").addEventListener("click", async () => {
 
 const email = emailInput.value;
 const password = passwordInput.value;
 
-const { data, error } = await supabase.auth.signUp({
+const { data, error } = await client.auth.signUp({
 email,
 password
 });
 
 if(error){
-msg.innerText = "❌ Błąd rejestracji";
+console.log(error);
+msg.innerText = "❌ Błąd rejestracji: " + error.message;
 return;
 }
 
-msg.innerText = "✅ Konto utworzone — sprawdź email";
-
+msg.innerText = "✅ Konto utworzone (sprawdź email)";
 });
