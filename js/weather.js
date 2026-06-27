a80afb0cf113883c265955a85d5be3ba44939997
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-06-27 18:20:23 +0200

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,3 @@
-// 🌤️ WEATHER SYSTEM
-
-
 async function loadWeather(lat,lng){
 
 
@@ -9,83 +6,38 @@ try{
 
 const res = await fetch(
 
-`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation&timezone=auto`
+`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=auto`
 
 );
-
 
 
 const data = await res.json();
 
 
-
-if(!data.current){
-
-console.log("Brak danych pogody");
-
-return;
-
-}
+console.log("🌤️ Pogoda załadowana",data.current);
 
 
-
-const weather = data.current;
-
-
-
-// 🌡️ temperatura
 
 document.getElementById("weatherTemp").innerText =
+data.current.temperature_2m + " °C";
 
-Math.round(weather.temperature_2m)+"°C";
-
-
-
-// 💧 wilgotność
 
 document.getElementById("weatherHumidity").innerText =
+data.current.relative_humidity_2m + " %";
 
-weather.relative_humidity_2m+"%";
-
-
-
-// 💨 wiatr
 
 document.getElementById("weatherWind").innerText =
-
-Math.round(weather.wind_speed_10m)+" km/h";
-
-
-
-// 🌧️ opady
-
-document.getElementById("weatherRain").innerText =
-
-weather.precipitation+" mm";
-
-
-
-console.log(
-"🌤️ Pogoda załadowana",
-weather
-);
+data.current.wind_speed_10m + " km/h";
 
 
 
 }
-
 
 catch(e){
 
-
-console.log(
-"❌ Błąd pogody",
-e
-);
-
+console.log("❌ Pogoda błąd",e);
 
 }
-
 
 
 }
