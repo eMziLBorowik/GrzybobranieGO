d6600072223b5d237dc6a9d8502999ddb0851484
eMziLBorowik <xxnarutoxx22@gmail.com>
2026-07-05 19:11:06 +0200

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,66 +1,25 @@
 let map;
+let userLat = null;
+let userLng = null;
 let userMarker = null;
 
-function initMap() {
+map = L.map("map").setView([52.2, 21], 15);
 
-  if (map) return map; // już istnieje
-
-  map = L.map("map", {
-    zoomControl: true
-  }).setView([52, 19], 6);
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; OpenStreetMap"
-  }).addTo(map);
-
-  return map;
+L.tileLayer(
+"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+{
+    attribution:"© OpenStreetMap"
 }
+).addTo(map);
 
-// =========================
-// 🎯 CENTER USER
-// =========================
+window.mainMap = map;
 
-function centerOnUser(lat, lng) {
-  if (!map) return;
-  if (!lat || !lng) return;
-  map.setView([lat, lng], 16);
-}
+// WARSTWY APLIKACJI
+window.userLayer = L.layerGroup();
+window.forestLayer = L.layerGroup();
+window.routeLayer = L.layerGroup();
 
-// =========================
-// 👤 USER MARKER
-// =========================
-
-function updateUserMarker(lat, lng, followGPS) {
-
-  if (!map) return;
-
-  if (!userMarker) {
-    userMarker = L.marker([lat, lng]).addTo(map);
-    map.setView([lat, lng], 16);
-  } else {
-    userMarker.setLatLng([lat, lng]);
-
-    if (followGPS) {
-      map.setView([lat, lng], 16);
-    }
-  }
-}
-
-// =========================
-// 🔧 RESIZE FIX
-// =========================
-
-function fixMapSize() {
-  setTimeout(() => {
-    map?.invalidateSize(true);
-  }, 200);
-}
-
-// =========================
-// 🌍 EXPORT
-// =========================
-
-window.initMap = initMap;
-window.centerOnUser = centerOnUser;
-window.updateUserMarker = updateUserMarker;
-window.fixMapSize = fixMapSize;
+// DODANIE WARSTW DO MAPY
+window.userLayer.addTo(map);
+window.forestLayer.addTo(map);
+window.routeLayer.addTo(map);
